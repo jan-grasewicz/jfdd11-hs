de54b7c33f2a5c3c09663fa6724356fe19d569e1
Filip <finnfinito@gmail.com>
2019-01-24 16:39:55 +0100

    Final navigation + JS script for fixed position

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let nav = document.querySelector('nav');
-let section = document.getElementById('#features');
+let section = document.querySelector('#features');
 
-onscroll
+/* navbar fixed position function */
+
+function changeNavPosition(){
+    if(window.scrollY > (section.offsetTop-50)) { /* section offset - 50px */
+        nav.classList.add('js__fixed')
+    }else{
+        nav.classList.remove('js__fixed')
+    }
+}
+
+/*  event listeners for different window actions */
+
+window.addEventListener('load', changeNavPosition);
+window.addEventListener('resize', changeNavPosition);
+window.addEventListener('scroll', changeNavPosition);
