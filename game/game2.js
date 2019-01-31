906cf84dbc39f3c1c8d9b78c0e3cc00358d8e773
Robert Dopieralski <abc507446692@gmail.com>
2019-01-31 16:33:36 +0100

    player rotates and moves in all directions

--- game/game2.js
+++ game/game2.js
@@ -7,52 +7,84 @@ let game = {
     player: {
         rotation: 0,
         position: { x: 50, y: 50 },
-        speed: 1,
-        maxSpeed: 10,
+        speed: 0,
+        maxSpeed: 3,
         direction: { x: 0, y: 0 }, //Current Speed Vector 
-        acceleration: 0.35,
+        acceleration: 1,
         rotateLeft: false,
         rotateRight: false,
         moveForward: false,
         moveBackward: false,
-        rotationSpeed: 5,
+        rotationSpeed: 6,
         rotationInRadians: 0,
     }
 }
 
 //functions being launched here
 spawnPlayer()
-setInterval(animation,25)
+setInterval(animation, 25)
 
-function animation(){
+function animation() {
     rotation()
     rotationToRadians()
     computeDirection()
+    accelerate()
+    move()
+    
     //console.log(game.player.direction)
 }
 
-function computeDirection(){
-    game.player.direction.y =(-1)* game.player.speed * Math.sin(game.player.rotationInRadians)
+function accelerate() {
+    if (game.player.moveForward === false && game.player.moveBackward === false || game.player.moveForward === true && game.player.moveBackward === true) {
+        if (game.player.speed > 0) {
+            game.player.speed = game.player.speed - game.player.acceleration
+        }
+        if (game.player.speed < 0) {
+            game.player.speed = game.player.speed + game.player.acceleration
+        }
+    }
+    if (game.player.moveForward === true && game.player.moveBackward === false) {
+        if (game.player.speed <= game.player.maxSpeed) {
+            game.player.speed += game.player.acceleration
+        }
+    }
+    if (game.player.moveBackward === true && game.player.moveForward === false) {
+        if (game.player.speed >= (-1 * game.player.maxSpeed)) {
+            game.player.speed -= game.player.acceleration
+        }
+    }
+    console.log(game.player.speed)
+}
+
+function move() {
+    game.player.position.x += (game.player.direction.x * Math.abs(game.player.speed))
+    game.player.position.y += game.player.direction.y * Math.abs(game.player.speed)
+    player.style.top = game.player.position.y + "px"
+    player.style.left = game.player.position.x + "px"
+}
+
+function computeDirection() {
+    game.player.direction.y = game.player.speed * Math.sin(game.player.rotationInRadians)
     game.player.direction.x = game.player.speed * Math.cos(game.player.rotationInRadians)
 }
 
-function rotationToRadians(){
-    game.player.rotationInRadians = game.player.rotation*Math.PI * (1/180)
+function rotationToRadians() {
+    game.player.rotationInRadians = game.player.rotation * Math.PI * (1 / 180)
 }
 
 function rotation() {
     if (game.player.rotateLeft === true) {
         game.player.rotation += game.player.rotationSpeed
-        player.style.transform = "rotate(" + game.player.rotation + "deg)" 
-        if(game.player.rotation >=360){
-            game.player.rotation = 0    
+        player.style.transform = "rotate(" + game.player.rotation + "deg)"
+        if (game.player.rotation >= 360) {
+            game.player.rotation = 0
         }
     }
     if (game.player.rotateRight === true) {
         game.player.rotation -= game.player.rotationSpeed
-        player.style.transform = "rotate(" + game.player.rotation + "deg)" 
-        if(game.player.rotation <=(-360)){
-            game.player.rotation = 0    
+        player.style.transform = "rotate(" + game.player.rotation + "deg)"
+        if (game.player.rotation <= (-360)) {
+            game.player.rotation = 0
         }
     }
 }
